26035a21c80201b261ef5f6b4a8e8f45b9d99fdd
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2021-01-13 13:03:54 -0500

    Create getVINData function

--- QuickVIN/VINLookup.swift
+++ QuickVIN/VINLookup.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+func getVINData(vin: String){
+    
+    //create the url with NSURL
+    let url = URL(string: "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/\(vin)*BA?format=json")!
+    let session = URLSession.shared
+    let request = URLRequest(url: url)
+
+    let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+        guard error == nil else {
+            return
+        }
+        guard let data = data else {
+            return
+        }
+       do {
+          //create json object from data
+          if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+             print(json)
+          }
+       } catch let error {
+         print(error.localizedDescription)
+       }
+    })
+
+    task.resume()
+
+}
