717f2235516dc9c80577c24fd8799da0e7f2b67a
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2021-01-13 12:46:13 -0500

    Initial Commit

---
+++ QuickVIN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickVIN
+//
+//  Created by Jordan Vaglivelo on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickVIN/QuickVINApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickVINApp.swift
+//  QuickVIN
+//
+//  Created by Jordan Vaglivelo on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickVINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
