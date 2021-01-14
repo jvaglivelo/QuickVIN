08f7ff4abc3840a7714adceddf8c444793d68b88
Jordan Vaglivelo <jordanvaglivelo@gmail.com>
2021-01-13 19:51:36 -0500

    Finalize SettingsView

--- QuickVIN/SettingsView.swift
+++ QuickVIN/SettingsView.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.openURL) var openURL
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("QuickVIN")
+                    .fontWeight(.black)
+                    .font(.system(size: 28))
+                    .foregroundColor(.blue)
+                    .padding()
+                
+            }
+            Text("Settings")
+                .fontWeight(.bold)
+                .font(.system(size: 22))
+                .foregroundColor(.blue)
+                .padding(.bottom)
+
+            VStack {
+                Text("QuickVIN quickly fetches basic data from a vehicle's VIN and displays it in an easy to read list")
+                    .padding()
+                    .multilineTextAlignment(.center)
+            }
+
+            HStack {
+                Text("Data Source")
+                    .padding()
+                Spacer()
+                Button(action: {
+                    openURL(URL(string: "https://vpic.nhtsa.dot.gov/api/")!)
+                }) {
+                    Text("NHTSA.DOT.GOV")
+                        .padding()
+                }
+            }
+            HStack {
+                Text("Developed by")
+                    .padding()
+                Spacer()
+                Button(action: {
+                    openURL(URL(string: "https://github.com/jvaglivelo")!)
+                }) {
+                    Text("Jordan Vaglivelo")
+                        .padding()
+                }
+            }
+            VStack {
+                Text("This Project is open source on GitHub")
+                    .padding(.top)
+                Button(action: {
+                    openURL(URL(string: "https://github.com/jvaglivelo/quickVIN")!)
+                }) {
+                    Text("View Source Code")
+                }
+
+            }
+
+            Spacer()
+            Button(action: {
+                if let window = UIApplication.shared.windows.first {
+                    window.rootViewController = UIHostingController(rootView: ContentView())
+                    window.makeKeyAndVisible()
+                }
+            }) {
+                Text("Return")
+                    .frame(width: UIScreen.main.bounds.width * 0.9, height: 25, alignment: .center)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color.accentColor)
+                    .cornerRadius(8)
+
+            }
+
+        }
     }
 }
 
